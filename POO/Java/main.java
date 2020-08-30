8674ee0efac27b9d4660ac77fefb01eddb9246f5
SIRP <jorge.sirp@gmail.com>
2020-08-30 19:44:42 +0200

    POO: Clase 'Objetos. Dando vida a nuestras clases en Java y Python.'

--- POO/Java/main.java
+++ POO/Java/main.java
@@ -3,5 +3,17 @@ package Java;
 class Main{
     public static void main(final String[] args) {
         System.out.println("Hola mundo");
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andres Herrera";
+        car.passenger = 4;
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "QWE567";
+        car2.driver = "Andrea Herrera";
+        car2.passenger = 3;
+        car2.printDataCar();
+
     }
 }
--- POO/Python/main.py
+++ POO/Python/main.py
@@ -1,2 +1,14 @@
+from car import Car
+
 if __name__ == '__main__':
     print('Hola mundo')
+
+    car = Car()
+    car.license = "AMS234"
+    car.driver = "Andrés Herrera"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "AMS234"
+    car2.driver = "Andrés Herrera"
+    print(vars(car2))
