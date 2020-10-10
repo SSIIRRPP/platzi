c841d642f35e073f8c1ee4e75f6b3ee14c7b021b
temp1 <jorge.sirp@gmailcom>
2020-10-10 17:02:20 +0200

    Correcion nombre carpeta para compatibilidad con windows

--- sql/eliminar "platziblog."/eliminar.py
+++
@@ -1,7 +0,0 @@
-with open("./datos.sql", "r") as f:
-    texto = f.read()
-    texto2 = texto.replace('platziblog.', 'platziblog' + "`" + "." + "`")
-    texto3 = texto.replace("`" + "," + "`", "`" + ", " + "`")
-
-with open("./datos.sql", 'w') as f2:
-    f2.write(texto3)
