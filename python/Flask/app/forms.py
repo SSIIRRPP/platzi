37ee09fb0000600ec1474426ca5d35b65da6ac98
temp1 <jorge.sirp@gmailcom>
2020-11-21 15:05:44 +0100

    Final curso Flask

--- python/Flask/app/forms.py
+++ python/Flask/app/forms.py
@@ -6,3 +6,13 @@ class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+class ToDoForm(FlaskForm):
+    description = StringField('Descripción', validators=[DataRequired()])
+    submit = SubmitField('Crear')
+
+class DeleteToDoForm(FlaskForm):
+    submit = SubmitField('Borrar')
+
+class UpdateToDoForm(FlaskForm):
+    submit = SubmitField('Actualizar')
