24fecabcd7ab91cfc53b63097ae87c11fee9316c
SSIIRRPP <jorge.sirp@gmail.com>
2021-06-21 18:33:19 +0200

    Commit

---
+++ web/JavaScript/1React/redux-por-bedu/blog_platzi/src/components/General/Fatal.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Fatal = (props) => <h2 className="center rojo">Error: {props.error}</h2>;
+
+export default Fatal;
---
+++ web/JavaScript/1React/redux-por-bedu/blog_platzi/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
+
+import reducers from "./reducers";
+
+import "./css/index.css";
+import App from "./components/App";
+
+const store = createStore(
+  reducers, //Todos los reducers
+  {}, //Estado inicial
+  applyMiddleware(reduxThunk)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
